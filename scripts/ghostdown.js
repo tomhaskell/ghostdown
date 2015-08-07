fefa6618fd082f94e89841b450722f15bd6fd7a3
Tom Haskell <tom@thlabs.net>
2015-08-07 22:02:03 +0100

    added word count functionality; removed debug log statements

--- scripts/ghostdown.js
+++ scripts/ghostdown.js
@@ -4,7 +4,7 @@ require.config({
             name: 'codemirror',
             location: 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.5.0',
             main: 'codemirror.min'
-   }],
+        }],
     // for some reason, the mardown mode in codemirror is looking for the
     // master codemirror file in the wrong place. This map fixes it.
     map: {
@@ -30,23 +30,19 @@ require([
     "codemirror/mode/markdown/markdown.min"
 ], function (CodeMirror, showdown, Dropzone) {
     'use strict';
-    console.log("loaded: ");
-    console.log(CodeMirror);
-    console.log(showdown);
-    console.log(Dropzone);
 
-    var textarea = document.querySelector('#ghostdown-editor');
-    var preview = document.querySelector('#ghostdown-rendered');
-    var converter = new showdown.Converter();
-    var editor = CodeMirror.fromTextArea(textarea, {
-        mode: 'markdown',
-        tabMode: 'indent',
-        lineWrapping: true
-    });
+    var textarea = document.querySelector('#ghostdown-editor'),
+        preview = document.querySelector('#ghostdown-rendered'),
+        converter = new showdown.Converter(),
+        editor = CodeMirror.fromTextArea(textarea, {
+            mode: 'markdown',
+            tabMode: 'indent',
+            lineWrapping: true
+        });
 
 
     function updateImagePlaceholders() {
-        var imgPlaceholders = (Array.prototype.slice.call(document.querySelectorAll('ghostdown-rendered p'))).filter(function (p) {
+        var imgPlaceholders = (Array.prototype.slice.call(document.querySelectorAll('#ghostdown-rendered p'))).filter(function (p) {
             return (/^(?:\{<(.*?)>\})?!(?:\[([^\n\]]*)\])(?:\(([^\n\]]*)\))?$/gim).test(p.innerText);
         });
         Dropzone.autoDiscover = false;
@@ -58,18 +54,18 @@ require([
                 var dropzone = new Dropzone(element, {
                     url: "/content/upload",
                     success: function (file, response) {
-                        var holderP = file.previewElement.closest("p");
-                        var preList = Array.prototype.slice.call(document.querySelectorAll('.CodeMirror-code pre'));
-                        var imgHolderMarkdown = preList.filter(function (element) {
-                            return (/^(?:\{<(.*?)>\})?!(?:\[([^\n\]]*)\])(?:\(([^\n\]]*)\))?$/gim).test(element.textContent) && (element.querySelectorAll('span').length === 0);
-                        });
+                        var holderP = file.previewElement.closest("p"),
+                            preList = Array.prototype.slice.call(document.querySelectorAll('.CodeMirror-code pre')),
+                            imgHolderMarkdown = preList.filter(function (element) {
+                                return (/^(?:\{<(.*?)>\})?!(?:\[([^\n\]]*)\])(?:\(([^\n\]]*)\))?$/gim).test(element.textContent) && (element.querySelectorAll('span').length === 0);
+                            }),
+                            editorOrigVal = editor.getValue(),
+                            nth = 0,
+                            newMarkdown = editorOrigVal.replace(/^(?:\{<(.*?)>\})?!(?:\[([^\n\]]*)\])(:\(([^\n\]]*)\))?$/gim, function (match, i, original) {
+                                nth++;
+                                return (nth === (index + 1)) ? (match + "(" + response.path + ")") : match;
+                            });
 
-                        var editorOrigVal = editor.getValue();
-                        var nth = 0;
-                        var newMarkdown = editorOrigVal.replace(/^(?:\{<(.*?)>\})?!(?:\[([^\n\]]*)\])(:\(([^\n\]]*)\))?$/gim, function (match, i, original) {
-                            nth++;
-                            return (nth === (index + 1)) ? (match + "(" + response.path + ")") : match;
-                        });
                         editor.setValue(newMarkdown);
 
                         holderP.classList.remove("dropzone");
@@ -85,12 +81,11 @@ require([
         updateImagePlaceholders();
     }
 
-    editor.on("change", function () {
-        updatePreview();
-        syncScroll();
-    });
-
-    updatePreview();
+    function updateWordCount() {
+        var text = document.querySelector('#ghostdown-rendered').innerHTML.replace(/(<([^>]+)>)/ig, '').trim(),
+            count = (text.length === 0) ? 0 : text.split(/\s+/).length;
+        document.querySelector('#ghostdown-wordcount').innerHTML = count + " words";
+    }
 
     function syncScroll() {
         var codeViewport = document.querySelector('.CodeMirror-scroll'),
@@ -103,14 +98,19 @@ require([
             previewHeight = previewContent.clientHeight - window.getComputedStyle(previewViewport, null).height.split("px")[0] + 50,
             ratio = previewHeight / codeHeight,
             previewPostition = codeViewport.scrollTop * ratio;
-        
+
 
         previewViewport.scrollTop = previewPostition;
-        
-        console.log( "code.clientHeight: "+codeContent.clientHeight+" , codeViewport.computedStyle: "+window.getComputedStyle(codeViewport, null).height );
-        console.log( "preview.clientHeight: "+previewContent.clientHeight+" , previewViewport.computedStyle: "+window.getComputedStyle(previewViewport, null).height );
-        console.log( "code.scrollTop: "+codeViewport.scrollTop+", preview.scrollTop: "+previewViewport.scrollTop);
     }
+
+    editor.on("change", function () {
+        updatePreview();
+        syncScroll();
+        updateWordCount();
+    });
+
+    updatePreview();
+    updateWordCount();
 
     document.querySelector('.CodeMirror-scroll').onscroll = syncScroll;
 });
